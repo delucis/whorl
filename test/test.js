c04b8d7d1abda105ccc1427823625fdecca0905d
delucis <swithinbank@gmail.com>
2018-08-17 15:18:24 +0200

    Rewrite tests to focus on functionality rather than sources

--- test/test.js
+++ test/test.js
@@ -1,100 +1,9 @@
 'use strict'
 
 const DESCRIBE = require('mocha').describe
-const AFTER = require('mocha').after
 const IT = require('mocha').it
 const EXPECT = require('chai').expect
-const TABLE = require('tty-table')
-const CHALK = require('chalk')
 const WHORL = require('../')
-
-// load test URLs from file
-const READ = require('read-yaml').sync
-const TEST_URLS = READ('./test/test-urls.yml')
-const METASCRAPER_URLS = READ('./test/metascraper-test-urls.yml')
-
-/**
- * Run tests for an array of objects containing url, author and (optionally)
- * site properties.
- * @param  {Object[]} urlArray          The array of objects to run tests with
- * @param  {String}   urlArray[].author The expected author for the url
- * @param  {String}   urlArray[].url    The url to fetch author metadata for
- * @param  {String=}  urlArray[].site  The name of the site being tested (printed in the test description)
- */
-function testUrlArray (urlArray) {
-  DESCRIBE('whorl', function () {
-    this.timeout(10000)
-
-    let rows = []
-    let testCount = 0
-    let passedCount = 0
-    let nullCount = 0
-    let failCount = 0
-
-    for (let test of urlArray) {
-      testCount++
-      let testDescription = `should return an author of “${test.author}” from ${test.site}`
-      IT(testDescription, async function () {
-        let author = await WHORL(test.url)
-        if (test.author) {
-          let authorRE = new RegExp('^' + test.author + '$', 'i')
-          let passes = authorRE.test(author)
-          if (passes) {
-            passedCount++
-          } else {
-            rows.push([
-              test.author,
-              CHALK.red(author || 'null')
-            ])
-            if (author === null) {
-              nullCount++
-            } else {
-              failCount++
-            }
-          }
-          EXPECT(author).to.match(authorRE)
-        } else {
-          passedCount++
-        }
-      })
-    }
-
-    AFTER(function () {
-      let header = [
-        {
-          value: 'expected',
-          width: 30
-        },
-        {
-          value: 'returned',
-          width: 30
-        }
-      ]
-      let percentPassed = passedCount / testCount * 100
-      let footerString = `${passedCount}/${testCount} (${percentPassed.toFixed(2)}%), ${nullCount} null, ${failCount} misidentified`
-      if (percentPassed < 50) {
-        footerString = CHALK.bgRed(footerString)
-      } else if (percentPassed < 75) {
-        footerString = CHALK.bgYellow(footerString)
-      } else {
-        footerString = CHALK.bgGreen(footerString)
-      }
-      footerString = CHALK.bold.black(footerString)
-      let footer = [
-        CHALK.bold('PASSED:'),
-        footerString
-      ]
-      let table = new TABLE(header, rows, footer, {
-        color: 'grey',
-        borderStyle: 0,
-        footerColor: 'white',
-        headerColor: 'white'
-      })
-
-      console.log(table.render())
-    })
-  })
-}
 
 DESCRIBE('whorl', function () {
   this.timeout(10000)
@@ -103,8 +12,56 @@ DESCRIBE('whorl', function () {
     let author = await WHORL('the-story-of-a-new-name')
     EXPECT(author).to.be.null
   })
+
+  IT('should return null if the URL does not return data', async () => {
+    let a = await WHORL('http://www.fake-url.net/')
+    EXPECT(a).to.be.null
+  })
+
+  IT('should return an article author from schema.org markup', async () => {
+    let a = await WHORL('http://www.nytimes.com/1982/12/19/books/the-poet-who-found-her-own-way.html')
+    EXPECT(a).to.equal('Rosemary Daniell')
+  })
+
+  IT('should return an article author from JSON-LD markup', async () => {
+    let a = await WHORL('http://www.newyorker.com/tech/elements/why-your-name-matters')
+    EXPECT(a).to.equal('Maria Konnikova')
+  })
+
+  IT('should return an article author from <meta> tags', async () => {
+    let a = await WHORL('http://www.nytimes.com/1994/08/19/nyregion/a-matter-of-identity-what-s-in-a-name-change-everything-you-can-imagine.html')
+    EXPECT(a).to.equal('Jennifer Steinhauer')
+  })
+
+  IT('should return a clean author name if the data includes “by,” “von” etc.', async () => {
+    let a = await WHORL('https://www.nzz.ch/feuilleton/elena-ferrantes-neapel-saga-in-der-schuld-des-ungluecks-ld.141596')
+    EXPECT(a).to.equal('Franz Haas')
+  })
+
+  IT('should handle multiple authors', async () => {
+    let a = await WHORL('http://www.lefigaro.fr/livres/2016/12/02/03005-20161202ARTFIG00156--le-nouveau-nom-d-elena-ferrante-sacre-meilleur-livre-de-l-annee.php')
+    EXPECT(a).to.equal('AFP agence and Elena Scappaticci')
+  })
+
+  IT('should prefer authors with an articleBody over authors without', async () => {
+    let a = await WHORL('https://www.theatlantic.com/technology/archive/2011/08/why-facebook-and-googles-concept-of-real-names-is-revolutionary/243171/')
+    EXPECT(a).to.equal('Alexis C. Madrigal')
+  })
+
+  DESCRIBE('lib/twitter.js (Twitter handling)', function () {
+    IT('should return a handle from a status', async () => {
+      let a = await WHORL('https://twitter.com/npmjs/status/590307732042973184')
+      EXPECT(a).to.equal('@npmjs')
+    })
+
+    IT('should return a handle from a profile', async () => {
+      let a = await WHORL('https://twitter.com/npmjs')
+      EXPECT(a).to.equal('@npmjs')
+    })
+
+    IT('should return null if not a status or profile', async () => {
+      let a = await WHORL('https://twitter.com/search')
+      EXPECT(a).to.be.null
+    })
+  })
 })
-
-testUrlArray(TEST_URLS)
-
-testUrlArray(METASCRAPER_URLS)
